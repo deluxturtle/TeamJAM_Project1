ff90adac8489c9523c70d5445801245ad8854deb
JakeSkov <JakeSkov95@gmail.com>
2015-08-24 17:27:07 -0500

    Revert "Setup"
    This reverts commit 44a4f117d51b1929549715efe749a666383e1847.

---
+++ SpaceProject/Assets/Scripts/ScriptCameraMouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptCameraMouse : MonoBehaviour
+{
+
+    public float lookSpeed = 200.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(GameObject.Find("Player").transform.position);
+
+        if (Input.GetMouseButton(1))
+        {
+            StartCoroutine("MouseMovement");
+
+        }
+
+    }
+
+    IEnumerator MouseMovement()
+    {
+        transform.LookAt(GameObject.Find("Player").transform.position);
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.RotateAround(GameObject.Find("Player").transform.position, new Vector3(mouseY, mouseX, 0), lookSpeed * Time.deltaTime);
+        yield return null;
+    }
+}
---
+++ SpaceProject/Assets/Scripts/ScriptRotateAround.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptRotateAround : MonoBehaviour {
+
+    public GameObject target;
+    [Range(1,360)]
+    public float orbitSpeed = 50.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(target.transform.position, Vector3.up, orbitSpeed * Time.deltaTime);
+	}
+}
